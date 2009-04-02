49b008c4309e2edd5ce5e48c235483ca7cd7130e
Luke Smith <lsmith@yahoo-inc.com>
2009-04-02 13:41:35 -0700

    Changing some Selector.query calls to getEBCN or even doc.getEBTN.  Moving some things out of batch

--- tests/yui-tests.js
+++ tests/yui-tests.js
@@ -13,13 +13,14 @@ window.tests = {
 	},
 	
 	"indexof" : function(){
-		// @TODO this is ugly :-o
-		var target, uls, index;
-		for ( var i = 0; i < 20; i++ ) {
+		var target, uls, index, i, j, l;
+
+		for ( i = 0; i < 20; i++ ) {
 			target = YAHOO.util.Dom.get('setid150');
 			uls = YAHOO.util.Selector.query('ul');
-			index = 0;
-			for ( var j = 0; j < uls.length; j++ ) {
+
+			index = -1;
+			for ( j = 0, l = uls.length; j < l; ++j ) {
 				if ( uls[j] === target ) {
 					index = j;
 					break;
@@ -30,47 +31,61 @@ window.tests = {
 	},
 	
 	"bind" : function(){
-		var lis = YAHOO.util.Selector.query('ul > li');
-		YAHOO.util.Event.addListener(lis, 'click', function(){});
+		var lis = YAHOO.util.Selector.query('ul > li:first-child');
+		YAHOO.util.Event.on(lis, 'click', function(){});
 		return lis.length;
 	},
 	
 	"attr" : function(){
-		return YAHOO.util.Dom.batch(YAHOO.util.Selector.query('ul'), function(el){
-			return YAHOO.util.Dom.getAttribute(el, 'id');
-		}).length;
+		return YAHOO.util.Dom.batch(
+            YAHOO.util.Selector.query('ul'),
+            function(el){
+                return YAHOO.util.Dom.getAttribute(el, 'id');
+		    }).length;
 	},
 	
 	"bindattr" : function(){
-		// @TODO See if we can speed this one up
-		var lis = YAHOO.util.Selector.query('ul > li');
-		YAHOO.util.Event.addListener(lis, 'mouseover', function(){});
-		YAHOO.util.Dom.batch(lis, function(li){ YAHOO.util.Dom.setAttribute(li, 'rel', 'touched'); });
-		YAHOO.util.Event.removeListener(lis, 'mouseover');
+		var lis = YAHOO.util.Selector.query('ul > li:first-child'),i;
+
+        YAHOO.util.Event.on(lis, 'mouseover', function(){});
+
+		for (i = lis.length - 1; i >= 0; --i) {
+            YAHOO.util.Dom.setAttribute(lis[i], 'rel', 'touched');
+        }
+
+        YAHOO.util.Event.removeListener(lis, 'mouseover');
 		return lis.length;
 	},
 
 	"table": function(){
-		var table;
-		for ( var i = 0; i < 40; i++ ) {
-			table = document.createElement('table');
+		var _, table, i;
+		for ( i = 0; i < 40; i++ ) {
+            _ = document.createElement('div');
+            _.innerHTML = '<table><tbody><tr><td>first</td></tr></tbody></table>';
+			table = _.getElementsByTagName('table')[0];
 			YAHOO.util.Dom.addClass(table, 'madetable');
+
 			document.body.appendChild(table);
-			table.innerHTML = '<tr><td>first</td></tr>';
-			YAHOO.util.Dom.insertBefore(document.createElement('td'), YAHOO.util.Selector.query('td', table, true));
+			YAHOO.util.Dom.insertBefore(
+                document.createElement('td'),
+                table.getElementsByTagName('td')[0]);
 		}
+
 		return YAHOO.util.Selector.query('tr td').length;
 	},
 	
 	"addanchor" : function(){
-		return YAHOO.util.Dom.batch(YAHOO.util.Selector.query('.fromcode > li'), function(li){
-			li.innerHTML = '<a href="http://example.com">link</a>';
-		}).length;
+		return YAHOO.util.Dom.batch(
+            YAHOO.util.Selector.query('.fromcode > li:first-child'),
+            function(li){
+			    li.innerHTML = '<a href="http://example.com">link</a>';
+		    }).length;
 	},
 
 	"append" : function(){
-		var div;
-		for ( var i = 0; i < 500; i++ ) {
+        // TODO: verify against other lib tests.  Confusing example docs.
+		var div, o;
+		for ( i = 0; i < 500; i++ ) {
 			div = document.createElement('div');
 			YAHOO.util.Dom.setAttribute(div, 'rel', 'foo2');
 			document.body.appendChild(div);
@@ -80,52 +95,61 @@ window.tests = {
 	
 	"addclass-odd" : function(){
 		var divs, odds;
-		divs = YAHOO.util.Selector.query('div');
+		divs = document.getElementsByTagName('div');
 		YAHOO.util.Dom.addClass(divs, 'added');
-		return YAHOO.util.Dom.batch(YAHOO.util.Selector.filter(divs, ':nth-child(even)'), function(div){
-			YAHOO.util.Dom.addClass(div, 'odd');
-		}).length;
+
+        odds = YAHOO.util.Selector.filter(divs, ':nth-child(odd)');
+        YAHOO.util.Dom.addClass(odds, 'odd');
+
+        return divs.length;
 	},
 	
 	"style": function(){
-		return YAHOO.util.Dom.batch(YAHOO.util.Selector.query('.added'), function(n){
-			YAHOO.util.Dom.setStyle(n, 'background-color', '#ededed');
-			YAHOO.util.Dom.setStyle(n, 'color', '#fff');
-		}).length;
+        var nodes = YAHOO.util.Dom.getElementsByClassName('added');
+        YAHOO.util.Dom.setStyle(nodes,'background-color', '#ededed');
+        YAHOO.util.Dom.setStyle(nodes,'color', '#fff');
+		return nodes.length;
 	},
 	
 	"removeclass": function(){
-		return YAHOO.util.Dom.batch(YAHOO.util.Selector.query('.added'), function(n) {
-			YAHOO.util.Dom.removeClass(n, 'added');
-		}).length;
+        var nodes = YAHOO.util.Dom.getElementsByClassName('added');
+		YAHOO.util.Dom.removeClass(nodes, 'added');
+		return nodes.length;
 	},
 	
 	"sethtml": function(){
-		return YAHOO.util.Dom.batch(YAHOO.util.Selector.query('div'), function(div){
-			div.innerHTML = '<p>new content</p>';
-		}).length;
+		return YAHOO.util.Dom.batch(
+            document.getElementsByTagName('div'),
+            function (div) {
+			    div.innerHTML = '<p>new content</p>';
+		    }).length;
 	},
 	
 	"insertbefore" : function(){
-		return YAHOO.util.Dom.batch(YAHOO.util.Selector.query('.fromcode a'), function(a){
-			var p = document.createElement('p');
-			p.innerHTML = 'A Link';
-			YAHOO.util.Dom.insertBefore(p,a);
-		}).length;
+		return YAHOO.util.Dom.batch(
+            YAHOO.util.Selector.query('.fromcode a'),
+            function(a){
+                var p = document.createElement('p');
+                p.innerHTML = 'A Link';
+                YAHOO.util.Dom.insertBefore(p,a);
+            }).length;
 	},
 	
 	"insertafter" : function(){
-		return YAHOO.util.Dom.batch(YAHOO.util.Selector.query('.fromcode a'), function(a){
-			var p = document.createElement('p');
-			p.innerHTML = 'After Link';
-			YAHOO.util.Dom.insertAfter(p,a);
-		}).length;
+		return YAHOO.util.Dom.batch(
+            YAHOO.util.Selector.query('.fromcode a'), function(a){
+                var p = document.createElement('p');
+                p.innerHTML = 'After Link';
+                YAHOO.util.Dom.insertAfter(p,a);
+            }).length;
 	},
 	
 	"destroy": function(){ 
-		return YAHOO.util.Dom.batch(YAHOO.util.Selector.query('.fromcode'), function(n){
-			n.parentNode.removeChild(n);
-		}).length;
+		return YAHOO.util.Dom.batch(
+            YAHOO.util.Dom.getElementsByClassName('fromcode'),
+            function(n){
+                n.parentNode.removeChild(n);
+            }).length;
 	},
 	
 	"finale": function(){
